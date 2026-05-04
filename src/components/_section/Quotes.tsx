9a0eedd1b9190aa18b9b51fbeb1ec362a78e4fe2
manikandanvanangamudi <manikandanv.jdev@gmail.com>
2026-05-04 18:49:19 -0400

    Update Quotes.tsx

--- src/components/_section/Quotes.tsx
+++ src/components/_section/Quotes.tsx
@@ -7,7 +7,7 @@ const Quotes = () => {
         <div className="bg-gray-950 w-[90%] md:w-[60%] lg:w-[40%] px-8 md:px-4 py-8 rounded-2xl flex flex-col gap-4">
           <p className="text-sm font-bold md:text-md lg:text-lg text-center">
             <RevealText
-              text="وَمِنْ كُلِّ شَيْءٍ خَلَقْنَا زَوْجَيْنِ لَعَلَّكُمْ تَذَكَّرُوْنَ"
+              text="Two souls walking as one."
               duration={0.3}
               stagger={0.15}
               mode="sentence"
@@ -16,7 +16,7 @@ const Quotes = () => {
           </p>
           <p className="text-sm font-bold md:text-md lg:text-lg text-center">
             <RevealText
-              text="Dan segala sesuatu Kami ciptakan berpasang-pasangan agar kamu mengingat (kebesaran Allah)"
+              text="May our journey be filled with harmony, love, and shared purpose — as we walk together in the seven sacred steps."
               duration={0.3}
               stagger={0.15}
               mode="sentence"
@@ -25,7 +25,7 @@ const Quotes = () => {
           </p>
           <p className="text-xs md:text-sm lg:text-md text-center italic">
             <RevealText
-              text="QS Az-Zariyat ayat 49"
+              text="From Sand to Snow, forever we GO"
               duration={0.3}
               stagger={0.15}
               mode="sentence"
