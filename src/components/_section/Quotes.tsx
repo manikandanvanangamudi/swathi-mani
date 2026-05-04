25ab7706ad90bc1d88321fa9fd3ef99bd334f7dd
manikandanvanangamudi <manikandanv.jdev@gmail.com>
2026-05-04 19:04:42 -0400

    Update Quotes.tsx

--- src/components/_section/Quotes.tsx
+++ src/components/_section/Quotes.tsx
@@ -11,7 +11,7 @@ const Quotes = () => {
     <div>
       <div className="flex flex-col items-center justify-center gap-2 h-[20vh] text-white">
         <div className="bg-gray-950 w-[90%] md:w-[60%] lg:w-[40%] px-8 md:px-4 py-8 rounded-2xl flex flex-col gap-4">
-          <p className="text-sm font-bold md:text-md lg:text-lg text-center ${secondFont.className}`}>
+          <p className={`text-sm font-bold md:text-md lg:text-lg text-center ${secondFont.className}`}>
             <RevealText
               text="Two souls walking as one."
               duration={0.3}
