c01e21d3276c156613ff4cd22240ebe3e24459ce
manikandanvanangamudi <manikandanv.jdev@gmail.com>
2026-05-04 19:01:42 -0400

    Update Quotes.tsx

--- src/components/_section/Quotes.tsx
+++ src/components/_section/Quotes.tsx
@@ -1,11 +1,17 @@
 import RevealText from "../ui/RevealText";
+import { Luxurious_Script } from "next/font/google";
+
+const secondFont = Luxurious_Script({
+  subsets: ["latin"],
+  weight: "400",
+});
 
 const Quotes = () => {
   return (
     <div>
       <div className="flex flex-col items-center justify-center gap-2 h-[20vh] text-white">
         <div className="bg-gray-950 w-[90%] md:w-[60%] lg:w-[40%] px-8 md:px-4 py-8 rounded-2xl flex flex-col gap-4">
-          <p className="text-sm font-bold md:text-md lg:text-lg text-center">
+          <p className="text-sm font-bold md:text-md lg:text-lg text-center ${secondFont.className}`}>
             <RevealText
               text="Two souls walking as one."
               duration={0.3}
