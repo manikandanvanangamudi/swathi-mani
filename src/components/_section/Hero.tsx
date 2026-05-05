6d1cb4301721fa84718d1c23e05bf52aa37b5974
manikandanvanangamudi <manikandanv.jdev@gmail.com>
2026-05-05 00:46:22 -0400

    Update Hero.tsx

--- src/components/_section/Hero.tsx
+++ src/components/_section/Hero.tsx
@@ -54,7 +54,7 @@ const Hero = ({ revealed = false }: HeroProps) => {
           src="/Hero.webp"
           alt="ring"
           fill
-          className="object-contain"
+          className="object-cover"
           priority
         />
         <div className="absolute inset-0 bg-black/50 z-10" />
